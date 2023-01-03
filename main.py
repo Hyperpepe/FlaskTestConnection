cb0703d26ff357cfcd2089329311306400e7c1ee
LXH <1411649658@qq.com>
2023-01-03 23:47:09 +0800

    添加ip以及信息检测

--- main.py
+++ main.py
@@ -6,45 +6,100 @@ import io
 import cv2
 import numpy as np
 import requests
-
-
+import socket
+import threading
 api = flask.Flask(__name__)
+
+
 def checkjpg(base64_code):
     image_data = base64.b64decode(base64_code)
     buf = io.BytesIO(image_data)
     buf = np.frombuffer(buf.getbuffer(), np.uint8)
     if cv2.imdecode(buf, cv2.IMREAD_COLOR):
         return
-def request_post(ip_address,port,bodyin):
-    host = "http://"+ip_address+":"+port+"/"
+def is_jpeg(base64_code):
+  try:
+    image_data = base64.b64decode(base64_code)
+    buf = io.BytesIO(image_data)
+    buf = np.frombuffer(buf.getbuffer(), np.uint8)
+    if cv2.imdecode(buf, cv2.IMREAD_COLOR):
+      return True
+  except Exception as e:
+    print(e)
+  return False
+
+def request_post(ip_address, port, bodyin):
+    host = "http://" + ip_address + ":" + port + "/"
     login_url = "/AICheckIsolaterjpg"
     url = host + login_url  # 拼接地址
     requests.post(url=url, json=bodyin)
 
+
+def check_args_IP(data):
+    # ret = False
+    if data:
+        operator = data['operator']
+        listen_port_info = data.get('listenPortInfo')# 读取 listenPortInfo 中的 IpAddress 和 Port
+        if listen_port_info:
+            ip_address = listen_port_info.get('IpAddress')
+            port = listen_port_info.get('Port')
+            if ip_address and port :
+                port = int(port)
+                # 创建一个 socket
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                # 连接到 IP 地址和端口
+                s.connect((ip_address, port))
+                print("IP 地址和端口可达")
+                s.close()
+                ret = True
+                return ret
+            else:
+                ret = False
+                return ret
+        else:
+            ret = False
+            return ret
+    else:
+        ret = False
+        return ret
+def check_args_IsolaterInfo(data):
+    isolater_info = data.get('IsolaterInfo')  # 读取 IsolaterInfo 中的 issueNumber、IsolaterID、IsolaterName 和 IsolaterCmd
+    if isolater_info:
+        issue_number = isolater_info.get('issueNumber')
+        isolater_id = isolater_info.get('IsolaterID')
+        isolater_name = isolater_info.get('IsolaterName')
+        isolater_cmd = isolater_info.get('IsolaterCmd')
+        isolater_cmd = int(isolater_cmd)
+        if all(value for value in isolater_info.values()):
+            print("IsolaterInfo 中所有值都有值")
+            if isolater_cmd.isnumeric() and (isolater_cmd == '1' or isolater_cmd == '2'):
+                print("isolater_cmd 是数字 1 或 2")
+                ret = True
+            else:
+                print("isolater_cmd 不是数字 1 或 2")
+                ret = False
+        else:
+            print("IsolaterInfo 中有值为空")
+            ret = False
+    else:
+        ret = False
+    return ret
+
+
+
 @api.route('/AICheckIsolaterjpg', methods=['post'])
 def chose():
     data = request.get_json()
-    # print(data)
-    operator = data['operator']
+    IPret = check_args_IP(data)
+    Isoret = check_args_IsolaterInfo(data)
+    isArgsOk = IPret and Isoret
+    if isArgsOk:
+        ren = {'msg': 'COPY_THAT', 'msg_code': 202}
+    else:
+        ren = {'msg': 'ERROR_NONE_ARGS', 'msg_code': 404}
 
-    # 读取 listenPortInfo 中的 IpAddress 和 Port
-    listen_port_info = data['listenPortInfo']
-    # isolater_info = data['IsolaterInfo']
-    pic_info = data['picinfo']
-    ip_address = listen_port_info['IpAddress']
-    port = listen_port_info['Port']
-    # 读取 picinfo 中的 APicInfo、BPicInfo 和 CPicInfo
-    a_pic_info = pic_info['APicInfo']
-    b_pic_info = pic_info['BPicInfo']
-    c_pic_info = pic_info['CPicInfo']
-    print(listen_port_info)
-
-
-    ren = {'msg': 'COPY_THAT','msg_code': 202}
-
-    print(r.text)
-    # ren = {'msg': 'ERROR_NONE_ARGS', 'msg_code': 404}
     return json.dumps(ren, ensure_ascii=False)
+
 
 @api.route('/test', methods=['post'])
 def test():
